276f9d8cfe8ad50355fd8852b42ed46afcca2664
Jon Wrona <jon@wrona.dev>
2025-05-24 03:23:56 -0500

    remove node types

--- packages/daggerheart-types/src/index.ts
+++ packages/daggerheart-types/src/index.ts
@@ -1,6 +1,3 @@
 // Re-export all types from the main source directory
 export * from "../../../src/types/daggerheart/character-sheet";
 export * from "../../../src/types/daggerheart/daggerheart";
-
-// Also add version info
-export const PACKAGE_VERSION = require("../package.json").version;
